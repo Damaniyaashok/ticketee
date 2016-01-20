19ed400505791c45da130a90680a5c86a77e4690
damaniyaashok <damaniyaashok@gmail.com>
2016-01-20 15:37:05 +0530

    implemented sign in

--- myapp/app/controllers/users_controller.rb
+++ myapp/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -17,8 +18,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user= User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "user profile updated successfully."
+      redirect_to projects_path
+    else
+      flash[:alert] = "user profile has not been updated."
+      render "edit"
+    end
+  end
+
   private
     def user_params
-      params.require(:user).permit(:name, :password, :password_confirmations)
+      params.require(:user).permit(:name, :email, :password, :password_confirmations)
     end
 end
